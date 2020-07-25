00f0d5963081d5a6a0bb78ffbbebc0949b1cccde
Georgeos Hardo <gh464@cam.ac.uk>
2020-07-26 00:20:55 +0100

    first_commit

---
+++ .ipynb_checkpoints/image_funcs-checkpoint.py
@@ -0,0 +1,53 @@
+from pystackreg import StackReg
+from skimage.filters import threshold_minimum, threshold_li
+import scipy
+import mahotas as mh
+import math
+import numpy as np
+from skimage.transform import hough_line, hough_line_peaks
+from skimage.feature import canny
+from skimage.transform import probabilistic_hough_line, hough_line
+from skimage.morphology import skeletonize
+import skimage
+
+def correct_drift(reference, move):
+    sr = StackReg(StackReg.RIGID_BODY)
+    
+    transformation_matrix = sr.register(reference, move)
+    
+    out_rot = sr.transform(move, transformation_matrix)
+    return out_rot, transformation_matrix
+
+def get_img_half(image, t_b):
+    height = int(image.shape[0]/2)
+    if t_b == "top":
+        return image[:height,:]
+    if t_b == "bottom":
+        return image[height:,:]
+    
+def get_orientation(image, debug = False):
+    bin_image = image > threshold_li(image) * 1
+    dilated = scipy.ndimage.morphology.binary_dilation(bin_image, iterations = 30)
+    labeled, num_regions  = mh.label(dilated)
+    sizes = mh.labeled.labeled_size(labeled)
+    mh.labeled.labeled_size(labeled)
+    too_small = np.where(sizes < sizes[1]-1)
+    labeled = mh.labeled.remove_regions(labeled, too_small)
+    skeleton = skeletonize(labeled)
+    h, theta, d = hough_line(skeleton)
+    origin = np.array((0, skeleton.shape[1]))
+    for _, angle, dist in zip(*hough_line_peaks(h, theta, d)):
+        y0, y1 = (dist - origin * np.cos(angle)) / np.sin(angle)
+    gradient = (y0-y1) /origin[1]
+    if debug == False:
+        return math.degrees(np.arctan(gradient))
+    elif debug == True:
+        f, ax = plt.subplots(figsize=(40,20))
+        plt.imshow(bin_image)
+        plt.show()
+        plt.imshow(dilated)
+        plt.show()
+        plt.imshow(skeleton)
+        plt.show()
+def fix_orientation(image, angle):
+    return skimage.transform.rotate(image = image, angle = -1*angle)
---
+++ .ipynb_checkpoints/nd2extractor_test-checkpoint.py
@@ -0,0 +1,123 @@
+import matplotlib.pyplot as plt
+import pprint
+from pims import ND2Reader_SDK
+pp = pprint.PrettyPrinter(indent=0)
+from tabulate import tabulate
+from tqdm import tqdm 
+import cv2
+from cowpy import cow
+from celluloid import Camera
+import colorama
+from termcolor import colored
+from joblib import Parallel, delayed
+
+cheese = cow.Turtle()
+msg = cheese.milk("BakshiLab FAST ND2 Extractor - By Georgeos Hardo")
+print(msg)
+
+def predict_t(directory): 
+    frames =  ND2Reader_SDK(directory)
+    frames.default_coords["t"] = 0
+    t = 0
+    while True:
+        try:
+            frames[t]
+            t = t+1
+        except:
+            break
+    frames.close()
+    return int(t)
+
+def predict_FOVs(directory):
+    frames =  ND2Reader_SDK(directory)
+    i = 1
+    frames.default_coords["m"] = 0
+    x_um = [frames[0].metadata["x_um"]]
+    y_um = [frames[0].metadata["y_um"]]
+    frames.default_coords["m"] = i
+    while (x_um[0] != frames[0].metadata["x_um"] or y_um[0] != frames[0].metadata["y_um"]):
+        frames.default_coords["m"] = i
+        x_um.append(frames[0].metadata["x_um"])
+        y_um.append(frames[0].metadata["y_um"])
+        i+=1
+    frames.close()
+    return int(len(x_um) - 1)
+    
+
+directory = input("Enter the absolute directory of the ND2 file ")
+
+save_directory = input("Enter the absolute directory to save the extracted files (with trailing /) ")
+
+# Get parameters of the experiment
+frames =  ND2Reader_SDK(directory)
+IMG_HEIGHT = frames.metadata["tile_height"]
+IMG_WIDTH = frames.metadata["tile_width"]
+IMG_CHANNELS_COUNT = frames.metadata["plane_count"]
+SEQUENCES = frames.metadata["sequence_count"]
+IMG_CHANNELS = []
+for x in tqdm(range(IMG_CHANNELS_COUNT), desc = "Getting experiment info - please wait"):
+    IMG_CHANNELS.append(frames.metadata["plane_{}".format(x)]["name"])
+frames.close()
+num_FOVs = predict_FOVs(directory)
+print(colored("I predict that there are {} FOVs in the experiment, is this correct? (y/n) ".format(num_FOVs), 'red', attrs=['bold']))
+predict_correct = input()
+if predict_correct == "y":
+    pass
+elif predict_correct == "n":
+    num_FOVs = int(input("Enter the correct number of FOVs"))
+
+print(colored("Predicting number of timepoints in experiment. Please wait", "red", attrs=["bold"]))
+num_t = predict_t(directory)
+print(colored("I predict that there are {} timepoints in the experiment, is this correct? (y/n) ".format(num_t), 'red', attrs=['bold']))
+
+predict_correct = input()
+if predict_correct == "y":
+    pass
+elif predict_correct == "n":
+    num_t = int(input("Enter the correct number of FOVs"))
+    
+assert int(SEQUENCES / num_FOVs) == num_t, "FOVs ({}) and timepoints ({}) do not match sequences ({}) in the experiment - check your inputs".format(num_FOVs,num_t,SEQUENCES)
+
+print("Experiment parameters (please verify before proceeding with extraction):")
+print(tabulate([
+    ['TIMEPOINTS', num_t],
+    ['FOVs', num_FOVs],
+    ['IMG_HEIGHT', IMG_HEIGHT], 
+    ['IMG_WIDTH', IMG_WIDTH],
+    ["IMG_CHANNELS_COUNT", IMG_CHANNELS_COUNT],
+    ['IMG_CHANNELS', IMG_CHANNELS]], headers=['Parameter', 'Value'], tablefmt='orgtbl'))
+
+
+
+print(colored("Choose image format: (TIFF/PNG) ", 'red', attrs=['bold']))
+
+img_format = input()
+
+
+FOVs_list = list(range(num_FOVs))
+CHANNELS_list = list(range(IMG_CHANNELS_COUNT))
+
+
+
+def save_image(frame, i, img_format):
+    if (img_format == "TIF") or (img_format == "TIFF"):
+        cv2.imwrite(save_directory + 'xy{}_T{}_{}.tif'.format(str(FOV).zfill(3),str(i).zfill(4),IMG_CHANNELS[channel]), frame, [cv2.IMWRITE_TIFF_COMPRESSION, 1])
+    if img_format == "PNG":
+        cv2.imwrite(save_directory + 'xy{}_T{}_{}.png'.format(str(FOV).zfill(3),str(i).zfill(4),IMG_CHANNELS[channel]), frame)
+
+with ND2Reader_SDK(directory) as frames:
+    for FOV in tqdm(FOVs_list, desc = "Overall (FOV) progress"):
+        for channel in tqdm(CHANNELS_list, desc = "Channel progress in FOV {}".format(FOV), leave = False):
+            frames.iter_axes = 't'
+            try:
+                frames.default_coords['c'] = channel
+            except:
+                pass
+            frames.default_coords['m'] = FOV
+            
+            Parallel(n_jobs=1, require='sharedmem')(delayed(save_image)(frames[i], i, img_format) for i in tqdm(range(num_t), desc = "Frame progress in channel {}".format(IMG_CHANNELS[channel]), leave = False) )
+            
+            #for i in tqdm(range(num_t), desc = "Frame progress in channel {}".format(IMG_CHANNELS[channel]), leave = False):
+            #    save_image(frames[i], i, img_format)
+                
+                
